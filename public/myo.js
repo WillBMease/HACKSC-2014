2299b71cb423de7687e988ad46dbb7f3b87e2ff5
Will Mease <wmease@usc.edu>
2014-11-08 03:35:37 -0800

    d

--- public/myo.js
+++ public/myo.js
@@ -216,8 +216,6 @@
 			socket_url  : "ws://127.0.0.1:10138/myo/"
 			// socket_url  : "ws://54.213.132.53:10138/myo/"
 			// socket_url  : "ws://54.213.132.53:8888/myo/"
-
-
 		},
 		events : [],
 		myos : [],
