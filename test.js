b5b47c2a8606f35804878ba71054c2b433d5235f
ryannorton <nort.ryan@gmail.com>
2014-11-08 01:11:46 -0800

    d

--- test.js
+++ test.js
@@ -9,14 +9,30 @@ myMyo.on('connected', function(){
     console.log('connected!', this.id)
 });
 
+var ready = false;
+
+console.log("Display message here: Calibrate your Myo!");
 myMyo.on('fingers_spread', function(edge){
     if(!edge) return;
-    console.log('Hello Myo!');
-    myMyo.vibrate();
+    if(ready == false) {
+	    myMyo.vibrate();
+	    myMyo.zeroOrientation();
+	    console.log("zeroed");
+	    ready = true;
+	    callback();
+	}
 });
 
-// myMyo.on('imu', function(data){
-// 	console.log(data);
-// }) 
-
+function callback() {
+	myMyo.on('orientation', function(data){
+	    if(Math.abs(data.x) > 0.4){
+	    	console.log(Math.abs(data.x));
+	        console.log('BANG!');
+	    }
+	});
+}
+// 	myMyo.on('imu', function(data){
+// 		console.log(data);
+// 	});
+// }
 console.log("hello world");
