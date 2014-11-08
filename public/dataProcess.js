1850c877980e0592b7eb6e5e22d945d81417cf74
ryannorton <nort.ryan@gmail.com>
2014-11-08 05:06:04 -0800

    pls work

--- public/dataProcess.js
+++ public/dataProcess.js
@@ -1,4 +1,10 @@
+
+
+//var ready2;
+
 function dataProcess(index, c){
+
+
 
 // var newer, older = 0
 var averagecount = 0; 
@@ -9,8 +15,8 @@ var averagecount = 0;
           // Receive the incoming message and play it calling midi function
             
 user[index].on('data', function(data){
-      if (data[0] == 0){
-        if (data[1] == 0){
+      if (data[0] == 0){        // latency measure
+        if (data[1] == 0){   // send ping back to user
           data[1] = 1
           for (var i = 0 ; i < 4 ; i++){
             if (user[i] != 0){
@@ -18,7 +24,7 @@ user[index].on('data', function(data){
           }
         }
       }
-        else if (data[1] == 1){
+        else if (data[1] == 1){     // receive ping back and calculate latency
           var newDate = +new Date()
           var latency = newDate - data[2]
 
@@ -38,9 +44,23 @@ user[index].on('data', function(data){
         
         }
       }
+
+      else if (data[0] == 1){       // receiving ready from other player
+        ready2 = true;
+        if(ready1 == true && ready2 == true && done == false)
+        {
+          done = true
+          counter();
+          console.log("START!");
+        }
+      }
       
         else if (data[0] == 2){
           console.log(data[1])
+        }
+
+        else if (data[0] == 3){
+          console.log('draw time of opponent: ' + data[1])
         }
 });
           user[index].on('close', function(err){ 
