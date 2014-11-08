eb6d1658770299cf53bd0b3d609aab80b32c845a
Will Mease <wmease@usc.edu>
2014-11-08 01:09:26 -0800

    d

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
 
 
-var http = require('http')
-, url  = require('url')
-, fs   = require('fs');
-var neededstats = [];
-//var p = __dirname + '/' + req.params.filepath; //there is some error here you haven't defined req yet
-http.createServer(function (req,res) {
-    if(req.url=='/index.html' || req.url=='/') {
-    fs.readFile('./index.html',function(err,data){
-        res.end(data);
-    });
-    } 
-   //  else {
-   //      var p = __dirname + '/' + req.params.filepath;
-   //      fs.stat(p, function(err, stats) {
-   //          if (err) {
-   //              throw err;
-   //          }
-   //      neededstats.push(stats.mtime);
-   //      neededstats.push(stats.size);
-   //      res.send(neededstats);
-   // });
-   // }
+// var http = require('http')
+// , url  = require('url')
+// , fs   = require('fs');
+// var neededstats = [];
+// //var p = __dirname + '/' + req.params.filepath; //there is some error here you haven't defined req yet
+// http.createServer(function (req,res) {
+//     if(req.url=='/index.html' || req.url=='/') {
+//     fs.readFile('./index.html',function(err,data){
+//         res.end(data);
+//     });
+//     } 
+//    //  else {
+//    //      var p = __dirname + '/' + req.params.filepath;
+//    //      fs.stat(p, function(err, stats) {
+//    //          if (err) {
+//    //              throw err;
+//    //          }
+//    //      neededstats.push(stats.mtime);
+//    //      neededstats.push(stats.size);
+//    //      res.send(neededstats);
+//    // });
+//    // }
 
-}).listen(8888);
-console.log('Server running.');
+// }).listen(8888);
+// console.log('Server running.');
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+   res.sendfile('./index.html');
+});
+
+app.use(express.static('public'));
+
+app.listen(8888);
+console.log('Listening on port 8888');
+
+
 
 
 // var http = require("http");
