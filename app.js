acf728dcdcbdbfac0169c045b2379d0073c11f77
Will Mease <wmease@usc.edu>
2014-11-08 00:47:59 -0800

    changing html name

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var server = http.createServer(function (request, response) {
   response.end("Hello World\n");
 });
 
+app.get('/', function(req, res){
+	res.sendfile('index.htm');
+});
+
 // Listen on port 8000, IP defaults to 127.0.0.1
 server.listen(8000);
 
