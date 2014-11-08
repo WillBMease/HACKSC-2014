53f22b39de5ee7ffc9bb5bbf0ee87f15208a7e39
System Administrator <root@guest-wireless-207-151-035-004.usc.edu>
2014-11-07 23:55:47 -0800

    Test.js

---
+++ test.js
@@ -0,0 +1,18 @@
+var Myo = require('myo');
+var myMyo = Myo.create();
+
+Myo.on('connected', function(){
+    console.log('connected!', this.id)
+});
+
+myMyo.on('fingers_spread', function(edge){
+    if(!edge) return;
+    console.log('Hello Myo!');
+    myMyo.vibrate();
+});
+
+myo.on('imu', function(data){
+	console.log(data);
+}) 
+
+console.log("hello world");
