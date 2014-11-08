2b242b63a8ed99efa6013dd59f79b002644222c4
Will Mease <wmease@usc.edu>
2014-11-08 00:50:01 -0800

    d

--- app.js
+++ app.js
@@ -1,18 +1,83 @@
-// Load the http module to create an http server.
-var http = require('http');
+// // Load the http module to create an http server.
+// var http = require('http');
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
-});
+// // Configure our HTTP server to respond with Hello World to all requests.
+// var server = http.createServer(function (request, response) {
+//   response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.end("Hello World\n");
+// });
 
-app.get('/', function(req, res){
-	res.sendfile('index.html');
-});
+// app.get('/', function(req, res){
+// 	res.sendfile('index.html');
+// });
 
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
+// // Listen on port 8000, IP defaults to 127.0.0.1
+// server.listen(8000);
 
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/");
+// // Put a friendly message on the terminal
+// console.log("Server running at http://127.0.0.1:8000/");
+
+
+// A very basic web server in node.js
+// Stolen from: Node.js for Front-End Developers by Garann Means (p. 9-10) 
+ 
+var port = 8000;
+var serverUrl = "127.0.0.1";
+ 
+var http = require("http");
+var path = require("path"); 
+var fs = require("fs"); 		
+ 
+console.log("Starting web server at " + serverUrl + ":" + port);
+ 
+http.createServer( function(req, res) {
+ 
+	var now = new Date();
+ 
+	var filename = req.url || "index.html";
+	var ext = path.extname(filename);
+	var localPath = __dirname;
+	var validExtensions = {
+		".html" : "text/html",			
+		".js": "application/javascript", 
+		".css": "text/css",
+		".txt": "text/plain",
+		".jpg": "image/jpeg",
+		".gif": "image/gif",
+		".png": "image/png"
+	};
+	var isValidExt = validExtensions[ext];
+ 
+	if (isValidExt) {
+		
+		localPath += filename;
+		path.exists(localPath, function(exists) {
+			if(exists) {
+				console.log("Serving file: " + localPath);
+				getFile(localPath, res, ext);
+			} else {
+				console.log("File not found: " + localPath);
+				res.writeHead(404);
+				res.end();
+			}
+		});
+ 
+	} else {
+		console.log("Invalid file extension detected: " + ext)
+	}
+ 
+}).listen(port, serverUrl);
+ 
+function getFile(localPath, res, mimeType) {
+	fs.readFile(localPath, function(err, contents) {
+		if(!err) {
+			res.setHeader("Content-Length", contents.length);
+			res.setHeader("Content-Type", mimeType);
+			res.statusCode = 200;
+			res.end(contents);
+		} else {
+			res.writeHead(500);
+			res.end();
+		}
+	});
+}
