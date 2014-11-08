fa0fb065a45a9514e3c54978a39ca6035a65c941
Will Mease <wmease@usc.edu>
2014-11-08 02:47:21 -0800

    d

--- app.js
+++ app.js
@@ -36,30 +36,30 @@ Myo.on('connected', function(){
     console.log('connected!', this.id)
 });
 
-// var ready = false;
+var ready = false;
 
-// console.log("Display message here: Calibrate your Myo!");
-// myMyo.on('fingers_spread', function(edge){
-//     if(!edge) return;
-//     if(ready == false) {
-// 	    myMyo.vibrate();
-// 	    myMyo.zeroOrientation();
-// 	    console.log("zeroed");
-// 	    ready = true;
-// 	    callback();
-// 	}
-// });
+console.log("Display message here: Calibrate your Myo!");
+myMyo.on('fingers_spread', function(edge){
+    if(!edge) return;
+    if(ready == false) {
+	    myMyo.vibrate();
+	    myMyo.zeroOrientation();
+	    console.log("zeroed");
+	    ready = true;
+	    callback();
+	}
+});
 
-// function callback() {
-// 	myMyo.on('orientation', function(data){
-// 	    if(Math.abs(data.x) > 0.4){
-// 	    	console.log(Math.abs(data.x));
-// 	        console.log('BANG!');
-// 	    }
+function callback() {
+	myMyo.on('orientation', function(data){
+	    if(Math.abs(data.x) > 0.4){
+	    	console.log(Math.abs(data.x));
+	        console.log('BANG!');
+	    }
+	});
+}
+// 	myMyo.on('imu', function(data){
+// 		console.log(data);
 // 	});
 // }
-// // 	myMyo.on('imu', function(data){
-// // 		console.log(data);
-// // 	});
-// // }
-// console.log("hello world");
+console.log("hello world");
--- public/myotest.js
+++ public/myotest.js
@@ -1,33 +1,33 @@
 
 
-// Myo.on('connected', function(){
-//     console.log('connected!', this.id)
+// // Myo.on('connected', function(){
+// //     console.log('connected!', this.id)
+// // });
+
+// var ready = false;
+
+// console.log("Display message here: Calibrate your Myo!");
+// myMyo.on('fingers_spread', function(edge){
+//     if(!edge) return;
+//     if(ready == false) {
+// 	    myMyo.vibrate();
+// 	    myMyo.zeroOrientation();
+// 	    console.log("zeroed");
+// 	    ready = true;
+// 	    callback();
+// 	}
 // });
 
-var ready = false;
-
-console.log("Display message here: Calibrate your Myo!");
-myMyo.on('fingers_spread', function(edge){
-    if(!edge) return;
-    if(ready == false) {
-	    myMyo.vibrate();
-	    myMyo.zeroOrientation();
-	    console.log("zeroed");
-	    ready = true;
-	    callback();
-	}
-});
-
-function callback() {
-	myMyo.on('orientation', function(data){
-	    if(Math.abs(data.x) > 0.4){
-	    	console.log(Math.abs(data.x));
-	        console.log('BANG!');
-	    }
-	});
-}
-// 	myMyo.on('imu', function(data){
-// 		console.log(data);
+// function callback() {
+// 	myMyo.on('orientation', function(data){
+// 	    if(Math.abs(data.x) > 0.4){
+// 	    	console.log(Math.abs(data.x));
+// 	        console.log('BANG!');
+// 	    }
 // 	});
 // }
-console.log("hello world");
+// // 	myMyo.on('imu', function(data){
+// // 		console.log(data);
+// // 	});
+// // }
+// console.log("hello world");
