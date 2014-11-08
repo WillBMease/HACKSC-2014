68a187f98554759ab08b54f7b1c5b93195788b51
Will Mease <wmease@usc.edu>
2014-11-08 01:46:35 -0800

    d

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 
 var Myo = require('myo');
-// var myMyo = Myo.create();
+var myMyo = Myo.create();
+
+// var myotest = require('./myotest.js');
+
 
 var express = require('express');
 var app = express();
+
 
 module.exports = function(config) {
   config.set({
@@ -23,3 +27,39 @@ app.use(express.static('public'));
 
 app.listen(8888);
 console.log('Listening on port 8888');
+
+
+
+
+
+Myo.on('connected', function(){
+    console.log('connected!', this.id)
+});
+
+var ready = false;
+
+console.log("Display message here: Calibrate your Myo!");
+myMyo.on('fingers_spread', function(edge){
+    if(!edge) return;
+    if(ready == false) {
+	    myMyo.vibrate();
+	    myMyo.zeroOrientation();
+	    console.log("zeroed");
+	    ready = true;
+	    callback();
+	}
+});
+
+function callback() {
+	myMyo.on('orientation', function(data){
+	    if(Math.abs(data.x) > 0.4){
+	    	console.log(Math.abs(data.x));
+	        console.log('BANG!');
+	    }
+	});
+}
+// 	myMyo.on('imu', function(data){
+// 		console.log(data);
+// 	});
+// }
+console.log("hello world");
