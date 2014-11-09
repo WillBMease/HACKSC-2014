4f1e9b8e550cf3226bcb4cd3a3e8c8d2f9533911
ryannorton <nort.ryan@gmail.com>
2014-11-09 06:56:40 -0800

    play again

--- public/myoUser.js
+++ public/myoUser.js
@@ -212,6 +212,12 @@ myo_ = {
 				console.log("you lost!");
 				$('#set').text('YOU LOSE!');
 			}
+			myo_.reset();
+		    clearInterval(s.yellowLight);
+		    clearInterval(s.redLight);			
+			s.myLives = 3;
+			s.opponentLives = 3;
+			myo_.primeMyo();
 		}
 		
 	},
