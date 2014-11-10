47869cf16a6da6cfec333c967d79b46bd9baad49
ryannorton <nort.ryan@gmail.com>
2014-11-10 03:26:58 -0600

    fix

--- public/ui.js
+++ public/ui.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     var first = '<div class="modal"><h1 class="threedee">Welcome to Gunslinger!</h1><a href="#" class="button playButton">Play!</a></div>';
     $('body').append(first);
-    // $('.playButton').click(function(){
+    $('.playButton').click(function(){
        $('.threedee').text('Searching For Gunslingers...').css('font-size','60px');
        $('.playButton').hide(); 
        $('.modal').append('<img height="200" src="img/sunny.svg" class="loader">');
@@ -21,7 +21,7 @@ $(document).ready(function(){
                 $('#health').show();
             }, 1000);
         }, 1000);
-    // });
+    });
 });
 
 
